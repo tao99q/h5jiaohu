6443d2763c45f4d0755a006b6517b3afaedcef81
zhanghongtao <zhanghongtao>
2017-06-21 23:33:16 +0800

    1

--- H5简历/js/index.js
+++ H5简历/js/index.js
@@ -27,7 +27,7 @@ var loadingRender = (function() {
 					step++;
 					$progressBox.css("width", step / total * 100 + "%");
 					if(step == total) {
-						if(page === 0)return;
+						if(page === 0) return;
 						window.setTimeout(function() {
 							$loading.css("display", "none");
 							phoneRender.init();
@@ -39,38 +39,72 @@ var loadingRender = (function() {
 	}
 })();
 /*phone显示 单例模式*/
-var phoneRender = (function(){
+var phoneRender = (function() {
 	var $phone = $("#phone"),
 		$listen = $phone.children(".listen"),
 		$listenTouch = $listen.children(".touch"),
 		$detail = $phone.children(".details"),
 		$detailTouch = $detail.children(".touch"),
 		$time = $phone.children(".time");
-	
+
 	var listenMusic = $("#listenMusic")[0],
 		detailMusic = $("#detailsMusic")[0],
 		musicTimer = null;
-	
-	return{
-		init:function(){
-			$phone.css("display","block");
+	//播放自我介绍的音频，并且计算时间(真实音频播放进度)
+	function detailMusicPlay() {
+		detailMusic.play();
+		musicTimer = window.setInterval(function() {
+			var curTime = detailMusic.currentTime,
+				minute = Math.floor(curTime / 60),
+				second = Math.floor(curTime - minute * 60);
+			minute < 10 ? minute = "0" + minute : null;
+			second < 10 ? second = "0" + second : null;
+			$time.html(minute + ":" + second);
+			//音频播放完成后关闭当前phone页面，打开message页面
+			if(detailMusic.duration === curTime) {
+				closePhone();
+			}
+		}, 1000);
+	}
+
+	function closePhone() {
+		window.clearInterval(musicTimer);
+		detailMusic.pause();
+		$phone.css("transform", "translateY(" + document.documentElement.clientHeight + "px)").on("webkitTransitionEnd", function() {
+			$phone.css("display", "none");
+		});
+		messageRender.init();
+	}
+	return {
+		init: function() {
+			$phone.css("display", "block");
 			listenMusic.play();
 			//给listenTouch绑定接听事件
 			//在移动端事件中，click会有300ms的延迟
 			//可以用移动端的专门事件的原生事件 touchstart touchmove touchend 来进行模拟，zepto中封装一个方法singleTap(单击)
-			$listenTouch.singleTap(function(){
-				$listen.css("display","none");
-				listenMusic.pause();
-				$detail.css("transform","translateY(0)");
-				$time.css("display","block");
-				detailMusic.play();
+			$listenTouch.singleTap(function() {
+				$listen.css("display", "none");
+				listenMusic.pause(); //停止音频播放
+				$detail.css("transform", "translateY(0)");
+				$time.css("display", "block");
+				detailMusicPlay();
 			});
+			$detailTouch.singleTap(closePhone);
 		}
 	}
 })();
-
+/*message显示 单例模式*/
+var messageRender = (function() {
+	var $message = $("#message");
+	return {
+		init: function() {
+			$message.css("display", "block");
+		}
+	}
+})();
 /*控制page显示*/
 var urlObj = window.location.href.queryURLParameter();
 var page = parseFloat(urlObj["page"]);
 isNaN(page) || page === 0 ? loadingRender.init() : null;
 page === 1 ? phoneRender.init() : null;
+page === 2 ? messageRender.init() : null;
